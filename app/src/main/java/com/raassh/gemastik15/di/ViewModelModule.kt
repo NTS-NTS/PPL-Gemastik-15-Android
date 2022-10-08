e1a2510d18e7de3e0c91006abaa167234f8c538c
Daanii Kusnanta <danynabilgk@gmail.com>
2022-10-08 21:27:02 +0700

    add place detail fragment

--- app/src/main/java/com/raassh/gemastik15/di/ViewModelModule.kt
+++ app/src/main/java/com/raassh/gemastik15/di/ViewModelModule.kt
@@ -4,6 +4,7 @@ import com.raassh.gemastik15.view.activity.main.MainActivityViewModel
 import com.raassh.gemastik15.view.fragments.discover.DiscoverViewModel
 import com.raassh.gemastik15.view.fragments.landing.LandingViewModel
 import com.raassh.gemastik15.view.fragments.login.LoginViewModel
+import com.raassh.gemastik15.view.fragments.placeDetail.PlaceDetailViewModel
 import com.raassh.gemastik15.view.fragments.register.RegisterViewModel
 import com.raassh.gemastik15.view.fragments.searchresult.SearchResultViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -32,5 +33,9 @@ val viewModelModule = module {
 
     viewModel {
         SearchResultViewModel()
+    }
+
+    viewModel {
+        PlaceDetailViewModel()
     }
 }
