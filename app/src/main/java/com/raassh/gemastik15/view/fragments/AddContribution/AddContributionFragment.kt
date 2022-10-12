60d0e2dd34eab1a807fedc17c6fc61b236c0baf5
raassh-23 <muh.nur.abdurrauf@gmail.com>
2022-10-12 12:57:23 +0700

    refactor AddContributionFragment class

--- app/src/main/java/com/raassh/gemastik15/view/fragments/AddContribution/AddContributionFragment.kt
+++ app/src/main/java/com/raassh/gemastik15/view/fragments/AddContribution/AddContributionFragment.kt
@@ -1,32 +1,39 @@
 package com.raassh.gemastik15.view.fragments.AddContribution
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.raassh.gemastik15.R
+import androidx.fragment.app.Fragment
+import com.raassh.gemastik15.databinding.FragmentAddContributionBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class AddContributionFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = AddContributionFragment()
-    }
-
-    private lateinit var viewModel: AddContributionViewModel
+    private val viewModel by viewModel<AddContributionViewModel>()
+    private var binding: FragmentAddContributionBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_add_contribution, container, false)
+        binding = FragmentAddContributionBinding.inflate(inflater, container, false)
+        return binding?.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AddContributionViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply {
+            //
+        }
+
+        viewModel.apply {
+            //
+        }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
 }
