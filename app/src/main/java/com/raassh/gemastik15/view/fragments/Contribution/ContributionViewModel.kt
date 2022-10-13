e54718730aa8af05c16c632b62c0308cb37ab5e8
Daanii Kusnanta <danynabilgk@gmail.com>
2022-10-13 23:53:45 +0700

    add contribution fragment layout

---
+++ app/src/main/java/com/raassh/gemastik15/view/fragments/Contribution/ContributionViewModel.kt
@@ -0,0 +1,7 @@
+package com.raassh.gemastik15.view.fragments.Contribution
+
+import androidx.lifecycle.ViewModel
+
+class ContributionViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
