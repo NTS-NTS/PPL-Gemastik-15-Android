43231d7ee8926083fce07af29b6aeccbceacba25
raassh-23 <muh.nur.abdurrauf@gmail.com>
2022-10-16 14:35:57 +0700

    better location tracking

--- app/src/main/java/com/raassh/gemastik15/view/activity/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/raassh/gemastik15/view/activity/dashboard/DashboardActivity.kt
@@ -3,19 +3,20 @@ package com.raassh.gemastik15.view.activity.dashboard
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.location.Location
 import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
 import android.provider.Settings
 import android.util.Log
 import androidx.activity.addCallback
+import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.location.*
 import com.raassh.gemastik15.R
 import com.raassh.gemastik15.databinding.ActivityDashboardBinding
 import com.raassh.gemastik15.utils.checkPermission
@@ -26,16 +27,19 @@ class DashboardActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDashboardBinding
     private val viewModel by viewModel<DashboardViewModel>()
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private var isTracking = false
 
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
     ) { permissions ->
         when {
             permissions[Manifest.permission.ACCESS_FINE_LOCATION] ?: false -> {
-                getMyLastLocation()
+                createLocationRequest()
             }
             permissions[Manifest.permission.ACCESS_COARSE_LOCATION] ?: false -> {
-                getMyLastLocation()
+                createLocationRequest()
             }
             else -> {
                 AlertDialog.Builder(this)
@@ -50,36 +54,13 @@ class DashboardActivity : AppCompatActivity() {
                         }
                         startActivity(intent)
 
-                        getMyLastLocation()
+                        createLocationRequest()
                     }
                     .setNegativeButton(getString(R.string.no)) { _, _ ->
                         finish()
                     }
                     .show()
             }
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun getMyLastLocation() {
-        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION) &&
-            checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
-        ) {
-            // TODO: refactor to get current location
-            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-                if (location != null) {
-                    viewModel.setLocation(location.latitude, location.longitude)
-                } else {
-                    Log.d("TAG", "getMyLastLocation: Location not found")
-                }
-            }
-        } else {
-            requestPermissionLauncher.launch(
-                arrayOf(
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                )
-            )
         }
     }
 
@@ -115,6 +96,107 @@ class DashboardActivity : AppCompatActivity() {
         }
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        getMyLastLocation()
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                val location = locationResult.locations.last()
+                viewModel.setLocation(location.latitude, location.longitude)
+            }
+        }
+        createLocationRequest()
+
+        viewModel.location.observe(this) {
+            Log.d("TAG", "onCreate: $it")
+        }
+    }
+
+    private val resolutionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.StartIntentSenderForResult()
+        ) { result ->
+            when (result.resultCode) {
+                RESULT_OK -> {
+                    startLocationUpdates()
+                }
+                RESULT_CANCELED -> {
+                    binding.root.showSnackbar(getString(R.string.turn_on_gps))
+                }
+            }
+        }
+
+    private fun createLocationRequest() {
+        if (!checkPermission(Manifest.permission.ACCESS_FINE_LOCATION) ||
+            !checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+        ) {
+            requestPermissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
+
+        locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000)
+            .build()
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+        val client = LocationServices.getSettingsClient(this)
+        client.checkLocationSettings(builder.build())
+            .addOnSuccessListener {
+                startLocationUpdates()
+            }
+            .addOnFailureListener { exception ->
+                if (exception is ApiException) {
+                    when (exception.statusCode) {
+                        LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> {
+                            try {
+                                val resolution = exception.status.resolution!!
+
+                                resolutionLauncher.launch(
+                                    IntentSenderRequest.Builder(resolution)
+                                        .build()
+                                )
+                            } catch (exc: Exception) {
+                                Log.d("TAG", "createLocationRequest: ${exc}")
+                                binding.root.showSnackbar(exc.message ?: getString(R.string.unknown_error))
+                            }
+                        }
+                        LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
+                            binding.root.showSnackbar(getString(R.string.turn_on_gps))
+                        }
+                    }
+                }
+            }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        isTracking = true
+        try {
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+        } catch (exception: SecurityException) {
+            isTracking = false
+            Log.e("TAG", "Error : " + exception.message)
+        }
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (isTracking) {
+            startLocationUpdates()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
     }
 }
